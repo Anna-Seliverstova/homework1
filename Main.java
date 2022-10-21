e56dd57161812168baf868f3c0cffe7bcdc52834
Anna-Seliverstova <116378196+Anna-Seliverstova@users.noreply.github.com>
2022-10-21 19:57:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 5;
+        int d = 7;
+        System.out.println(a * b * c * d);
+    }
+}
